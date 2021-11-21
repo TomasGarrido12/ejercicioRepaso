b63601be4591ed8a1c6fcd77d7b433402b04de8a
TomasGarrido12 <garrido.tomas12@gmail.com>
2021-11-21 14:18:30 -0300

    primer commit con parte del ejercicio hecho

---
+++ Repaso/BibliotecaDeClases/Estante.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaDeClases
+{
+    public class Estante
+    {
+        private int ubicacionDelEstante;
+        private Producto[] productos;
+
+        private Estante(int capacidad)
+        {
+            productos = new Producto[capacidad];
+        }
+
+        public Estante(int capacidad, int ubicacion) : this(capacidad)
+        {
+            this.ubicacionDelEstante = ubicacion;
+        }
+
+        public Producto[] GetProductos()
+        {
+            return productos;
+        }
+        public static string MostrarEstante(Estante e)
+        {
+            return ($"La ubicacion del estante es {e.ubicacionDelEstante} y el producto es {e.GetProductos()}");
+        }
+
+        public static bool operator ==(Estante e, Producto p)
+        {
+            bool noEstaEnElEstante = true;
+
+            foreach (Producto producto in e.productos)
+            {
+                if (producto == p)
+                {
+                    noEstaEnElEstante = false;
+                    break;
+                }
+            }
+            if (noEstaEnElEstante)
+            {
+                return noEstaEnElEstante;
+            }
+            return false;
+
+        }
+
+        public static bool operator !=(Estante e, Producto p)
+        {
+            bool EstaEnElEstante = false;
+
+            foreach (Producto producto in e.productos)
+            {
+                if (producto != p)
+                {
+                    EstaEnElEstante = true;
+                    break;
+                }
+            }
+            if (EstaEnElEstante)
+            {
+                return EstaEnElEstante;
+            }
+            return true;
+        }
+
+        public static bool operator +(Estante e, Producto p)
+        {
+            e.productos.Append(p);
+            return true;
+        }
+    }
+}
---
+++ Repaso/BibliotecaDeClases/Producto.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace BibliotecaDeClases
+{
+    public class Producto
+    {
+        private string codigoDeBarra;
+        private string marca;
+        private float precio;
+
+        public Producto(string codigoDeBarra, string marca, float precio)
+        {
+            this.codigoDeBarra = codigoDeBarra;
+            this.marca = marca;
+            this.precio = precio;
+        }
+
+        public string GetMarca()
+        {
+            return this.marca;
+        }
+
+        public float GetPrecio()
+        {
+            return this.precio;
+        }
+
+        public static string MostrarProducto(Producto p)
+        {
+            return ($"El producto {p.GetMarca()} tiene un precio de {p.GetPrecio()} y un codigo de barras de {p.codigoDeBarra}");
+        }
+
+        public static explicit operator string(Producto p)
+        {
+            return p.codigoDeBarra;
+        }
+
+        public static bool operator ==(Producto p1, Producto p2) // dos productos van a ser iguales si sus marcas y codigos de barras son iguales
+        {
+            string codigoDeBarra1 = p1.codigoDeBarra;
+            string codigoDeBarra2 = p2.codigoDeBarra;
+            return (p1.marca == p2.marca) && codigoDeBarra1 == codigoDeBarra2;  //preguntar por este metodo la parte de codigoDeBarra
+        }
+
+        public static bool operator !=(Producto p1, Producto p2)
+        {
+            return !(p1 == p2);
+        }
+
+        public static bool operator ==(Producto p, string marca) //van a ser igual siempre y cuando la marca de mi objeto sea igual a la marca que le pase como parametro
+        {
+            return p.marca == marca;
+        }
+
+        public static bool operator !=(Producto p, string marca)
+        {
+            return !(p == marca);
+        }
+    }
+}
